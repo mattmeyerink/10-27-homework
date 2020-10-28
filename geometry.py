f286e013fd959ec1ad503c51169a028689df4d74
Matthew Meyerink <meyerink@umich.edu>
2020-10-27 21:29:25 -0500

    Initializing repository

---
+++ geometry.py
@@ -0,0 +1,9 @@
+import math
+
+def square_footage(length, width):
+    """Calculates the square footage of a given room."""
+    return length * width
+
+def circumference(radius):
+    """Calculate the circumference of a circle."""
+    return 2 * math.pi * radius
